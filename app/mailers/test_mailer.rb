9e8bd099557f8979109bfd294fc2f17ea06c5875
T.J. Schuck <tj@getharvest.com>
2014-02-26 17:18:25 -0500

    Generate "rails g mailer test_mailer test_email"

---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,14 @@
+class TestMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.test_mailer.test_email.subject
+  #
+  def test_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
